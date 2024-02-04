3449c3257aee5ff3df81bddf3dd461bfd85fe176
Aliaksandra Ryzvanovich <aliaksandra.ryzvanovich@ukg.com>
2024-02-04 20:58:38 +0100

    Initial commit

---
+++ src/main/java/com/epam/ryzvanovich/backendservicesmentoring/BackendServicesMentoringApplication.java
@@ -0,0 +1,13 @@
+package com.epam.ryzvanovich.backendservicesmentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServicesMentoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServicesMentoringApplication.class, args);
+	}
+
+}
